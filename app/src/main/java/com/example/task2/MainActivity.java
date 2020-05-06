b0494302a81ae90706e7f9c50e3374dcb89ec029
vinayak1221 <thevcreations3@gmail.com>
2020-05-06 15:34:15 +0530

    change

--- app/src/main/java/com/example/task2/MainActivity.java
+++ app/src/main/java/com/example/task2/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.task2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
--- app/src/main/java/com/example/task2/myAdapter.java
+++ app/src/main/java/com/example/task2/myAdapter.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
